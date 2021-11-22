8649f1ce8e65de9b8be7e3b2f52f4206458274fc
Isonzo <renato.soverina@gmail.com>
2021-11-21 19:01:08 -0500

    Segmented up main.c to try make it easier to see what's going on.

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include "mine.h"
 #include <time.h>
+
+void initNcurses(void);
+Tile** initMine(Pos* cursor, Tile** board);
+void updateGame(Pos* cursor, Tile** board);
+void badEndGame(void);
+void goodEndGame(void);
+
 int main()
 {	
 	srand(time(NULL));
@@ -8,26 +15,14 @@ int main()
 	Pos* cursor;
 	bool game_won = false;
 
-	// ncurses init functions
-	initscr();
-	noecho();
-	cbreak();
-
-	// minesweeper init functions
-	cursor->y = 8;
-	cursor->x = 1;
-	board = generateMap();
-	placeMines(board);
+	initNcurses();
+	board = initMine(cursor, board);
 
 	while (!game_won)
 	{
 		if (getInput(ch, cursor, board))
 				{
-					// update the board
-					printMap(board);
-					updateCursor(cursor, board);
-					refresh();
-
+					updateGame(cursor, board);
 					// Check if board state is a winning one
 					game_won = isGameWon(board);
 					if (game_won)
@@ -36,9 +31,7 @@ int main()
 		else
 		{
 			// If getInput is false, it means the player has lost.
-			clear();
-			printw("You've been blown to pieces...");
-			getch();
+			badEndGame();
 			break;
 		}
 
@@ -47,12 +40,47 @@ int main()
 	}
 	// If player exited the loop having won, be sure to print out that they've won.
 	if (game_won)
-	{
-		clear();
-		printw("You won!");
-		getch();
-	}
+		goodEndGame();
 	endwin();	
 
 	return 0;
+}
+
+// ncurses init functions
+void initNcurses(void)
+{ initscr();
+	noecho();
+	cbreak();
+}
+
+// minesweeper init functions
+Tile** initMine(Pos* cursor, Tile** board)
+{
+	cursor->y = 8;
+	cursor->x = 8;
+	board = generateMap();
+	placeMines(board);
+
+	return board;
+}
+// update the board
+void updateGame(Pos* cursor, Tile** board)
+{
+	printMap(board);
+	updateCursor(cursor, board);
+	refresh();
+}
+
+void badEndGame(void)
+{
+	clear();
+	printw("You've been blown to pieces...");
+	getch();
+}
+
+void goodEndGame(void)
+{
+		clear();
+		printw("You won!");
+		getch();
 }
